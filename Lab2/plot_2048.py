ea8e2b0eaa7c5ee005e7bc7c420515b17f5331e5
icchiu <kevin8812107@gmail.com>
2023-04-01 10:11:07 +0800

    add lab2

---
+++ Lab2/plot_2048.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+episodes = []
+means = []
+
+with open("log_add_feature.txt", 'r', encoding='utf-8') as fp:
+    for line in fp.readlines():
+        s = line.split('\t')
+        if len(s) < 2:
+            continue
+        s1 = s[1].split(' ')
+        if s1[0]=="mean":
+            episodes.append(int(s[0]))
+            means.append(float(s1[2]))
+
+plt.plot(episodes, means)
+
+plt.xlabel('Episodes')
+plt.ylabel('mean')
+plt.grid()
+plt.savefig('score_add_feature_P.png')
+    
